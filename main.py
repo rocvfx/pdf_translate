ff2f85878220c3be2e69cc34df575869c08f000e
rocvfx <zp2000@sina.com>
2022-07-07 16:09:34 +0800

    调整快捷键，去除等待bug

--- main.py
+++ main.py
@@ -6,33 +6,44 @@ import pyperclip
 import pyautogui
 import sys
 import time
-from UI_pdf_translate_designed import Ui_pdf_translate
 
+
+
+from UI_pdf_translate import Ui_pdf_translate
+
+#pyautogui.PAUSE = 0.5
 
 class myWindow(QWidget, Ui_pdf_translate):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)  # 调用父类的setupUI函数
+        self.setupUi(self)
 
-        self.pushButton_translate.clicked.connect(self.format_pdf_text)  # 将按钮点击事件和helloworld函数绑定
+        self.pushButton_translate.clicked.connect(self.format_pdf_text)
 
     def format_pdf_text(self):
-
-        temp_text = pyperclip.waitForPaste()
+        temp_text = pyperclip.paste()
         #temp_text.replace("\r\n", " ")
-        clean_text = temp_text.replace('\n', '').replace('\r', '')
-        clean_text.strip()
+        clean_text = temp_text.replace('\n', ' ').replace('\r', ' ')
+        #clean_text.strip()
         self.plainTextEdit_pdf_text.setPlainText(clean_text)
         self.plainTextEdit_pdf_text.setFocus()
         time.sleep(0.2)
-        pyautogui.hotkey('ctrl', 'a')  # 按键ctrl+c
-        time.sleep(0.2)
-        pyautogui.hotkey('ctrl', 'f9')  # 按键ctrl+c
+        pyautogui.hotkey('ctrl', 'a')  # 按键ctrl+a 全选文本
+        time.sleep(0.5)
+        #pyautogui.hotkey('ctrl', 'c', 'c')  # 召唤deepl
+        pyautogui.keyDown('ctrl')
+        pyautogui.press(['c', 'c'])
+        pyautogui.keyUp('ctrl')
 
-
-
-if __name__ == "__main__":
+def main():
     app = QApplication(sys.argv)
     ui = myWindow()
     ui.show()
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
+    # app = QApplication(sys.argv)
+    # ui = myWindow()
+    # ui.show()
+    # sys.exit(app.exec_())
